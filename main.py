d0731387b2f1980828018775d3a0a4e9507d2acd
muckas <motobike8@outlook.com>
2021-02-01 15:59:37 +0500

    Add a function for converting chart to Heikin Ashi

--- main.py
+++ main.py
@@ -33,11 +33,35 @@ def getChartData(pair, period, start, end):
         })
   return chart
 
+def getHeikinAshi(pair, period, start, end):
+  data = getChartData(pair, period, start, end)
+  chart = []
+  chart.append(data[0])
+  for candle in data:
+    high = candle['high']
+    low = candle['low']
+    open = ( chart[-1]['open'] + chart[-1]['close'] ) / 2
+    close = ( open + high + low + candle['close'] ) / 4
+    if open > close:
+      color = 'red'
+    else:
+      color = 'green'
+    chart.append(
+        {
+          'date':int(candle['date']),
+          'open':open,
+          'close':close,
+          'high':high,
+          'low':low,
+          'color':color
+        })
+  return chart
+
 if __name__ == '__main__':
   now = int(time.time())
   pair = 'USDT_BTC'
   period = 86400
-  start = now - (period * 365)
+  start = now - (period * 365 * 5)
   end = now
-  for candle in getChartData(pair, period, start, end):
+  for candle in getHeikinAshi(pair, period, start, end):
     print(candle)
